3f9ec2768ae8bf823c8b531d33c5b1093af0be34
unknown <szitagergo@DESKTOP-07L9CPJ.c-help.hu>
2018-03-01 12:50:53 +0100

    alma

--- temperature/firstraspberry.py
+++ temperature/firstraspberry.py
@@ -10,13 +10,7 @@ title=['Time', 'Temperature', 'Humidity']
 redirect_uri = "http://localhost:8080/"
 client_secret = "b016ZXPsQ9aC9O5om9FCCXA"
 
-client = onedrivesdk.get_default_client(client_id='55ebd19b-941c-4779-8847-a661f1cac052', scopes=['wl.signin', 'wl.offline_access', 'onedrive.readwrite'])
-auth_url = client.auth_provider.get_auth_url(redirect_uri)
 
- # Block thread until we have the code
-code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
-# Finally, authenticate!
-client.auth_provider.authenticate(code, redirect_uri, client_secret)
 
 
 
@@ -32,23 +26,5 @@ try:
 		args=[datetime.datetime.now(),t,h]
 		writer.writerow(args)
 		time.sleep(10)
-finally:
-	ofile.close()
-
-	#My Application Id:
-#(55ebd19b-941c-4779-8847-a661f1cac052)
-	#My application secret password/key:
-#(b016ZXPsQ9aC9O5om9FCCXA)
-#Application Name:
-#AirQ
-
-returned_item = client.item(drive="me", id="root").children['air3.csv'].upload('air')
-
-
-#sys_bus = dbus.SystemBus()
-#ck_srv = sys_bus.get_object('org.freedesktop.ConsoleKit','/org/freedesktop/ConsoleKit/Manager')
-#ck_iface = dbus.Interface(ck_srv, 'org.freedesktop.ConsoleKit.Manager')
-#stop_method = ck_iface.get_dbus_method("Stop")
-#stop_method()
 
 	
